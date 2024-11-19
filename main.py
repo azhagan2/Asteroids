68e5e8f287e5af2faaee2fa447f44b984c27f287
azhagan2 <azhagans128@gmail.com>
2024-11-19 23:19:17 +0530

    player established

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
 
 def main():
-    pygame.init()
+    pygame.init()                           # Initialize the pygame module
+    clock = pygame.time.Clock()             # Initialize the pygame.time.Clock 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))  # Set the screen using the pygame.display.set_mode()
 
-    
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)    # player object is created 
+    dt = 0
 
     while(1):
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return  
+                
+
+        screen.fill("black")      # fill the screen with black
         
-        pygame.Surface.fill(screen,(0,0,0))
+        for i in updatable:
+            i.update(dt)       # will re-render players on each time frame 
 
-        pygame.display.flip()
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        for i in drawable:
+            i.draw(screen)     # will update when we press keys (w,s,a,d)
 
+        pygame.display.flip()     # using flip method to refresh the screen
+
+        dt = clock.tick(60) / 1000     # limit the framerate to 60 FPS
 
 
 if __name__ == "__main__":
