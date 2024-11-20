1bbc944581979020841360a00888f9ce340da956
azhagan2 <azhagans128@gmail.com>
2024-11-20 23:46:22 +0530

    Last-1 step

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+import sys
 from constants import *
 from player import Player
 from circleshape import CircleShape
+from asteroidfield import AsteroidField
+from asteroid import Asteroid
+from shot import Shot
 
 def main():
     pygame.init()                           # Initialize the pygame module
@@ -11,11 +15,21 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+    
 
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)    # player object is created 
     dt = 0
+
+    player.timer = 0
+
+    asteroid_field = AsteroidField()
 
     while(1):
         for event in pygame.event.get():
@@ -27,6 +41,17 @@ def main():
         
         for i in updatable:
             i.update(dt)       # will re-render players on each time frame 
+
+        for i in asteroids:
+            if i.collision(player):
+                sys.exit("Game over!")
+            
+            for j in shots:
+                if j.collision(i):
+                    i.kill()
+                    j.kill()
+
+        
 
         for i in drawable:
             i.draw(screen)     # will update when we press keys (w,s,a,d)
